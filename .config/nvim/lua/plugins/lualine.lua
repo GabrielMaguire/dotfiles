05af33b04f11a0f7d34f00393f6817cf8195983b
Gabriel Maguire <gabrielmaguire@proton.me>
2024-01-15 22:42:11 -0500

    NeoVim: Re-organized lualine to remove time widget.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,12 @@ return {
   "nvim-lualine/lualine.nvim",
   optional = true,
 
-  -- Remove Aerial integration
   opts = function(_, opts)
+    -- Remove Aerial integration
     table.remove(opts.sections.lualine_c)
+
+    -- Re-organize sections
+    opts.sections.lualine_y = { "progress" }
+    opts.sections.lualine_z = { "location" }
   end,
 }

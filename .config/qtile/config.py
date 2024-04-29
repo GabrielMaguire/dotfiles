a855fd1d76f2c10ef37c8064c5f044df210c4e2f
Gabriel Maguire <gabrielmaguire@proton.me>
2024-04-28 21:00:07 -0400

    qtile: Volume percentage change, browser keymap.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -30,6 +30,7 @@ from libqtile.lazy import lazy
 
 mod = "mod4"
 terminal = "kitty"
+browser = "firefox"
 
 keys = [
     # A list of available commands that can be bound to keys can be found
@@ -39,13 +40,14 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
@@ -53,6 +55,7 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -63,17 +66,35 @@ keys = [
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+    # Screen and layout management
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "s", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Launch Rofi dmenu"),
     Key([mod], "f", lazy.window.toggle_floating(), desc="Toggle floating window"),
     Key([mod], "m", lazy.window.toggle_maximize(), desc="Toggle maximize window"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key(["mod1"], "Tab", lazy.next_screen(), desc="Focus next screen"),
+
+    # Specific application launchers
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
+    Key([mod], "s", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Launch Rofi dmenu"),
+
+    # Qtile config keymaps
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
+    # Volume keys
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -2%")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +2%")),
+    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
+
+    # Backlight keys
+    Key([], "XF86MonBrightnessUp", lazy.spawn("xbacklight +10")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -10")),
+
+    # Key([mod, "shift"], "e", lazy.function(volume_popup)),
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -142,7 +163,9 @@ screens = [
                     format="{char} {percent:2.0%}",
                     low_percentage=0.15,
                     charge_char="󰂄",
-                    discharge_char=""
+                    full_char="󰁹",
+                    discharge_char="",
+                    show_short_text=False,
                 ),
                 widget.Spacer(length=20),
                 widget.Clock(format="%Y-%m-%d %I:%M:%S %p"),
